72f2c09d4b1f52151f87d4480570337d57e03447
Kien <dvkien2803@gmail.com>
2020-11-09 11:38:31 +0700

    up load

--- src/Component/index.jsx
+++ src/Component/index.jsx
@@ -4,12 +4,13 @@ import { gsap } from 'gsap'
 function Component(props) {
     useEffect(() => {
         const timeLine = gsap.timeline()
-        timeLine.from(".welcome", { x: "-50%", scale: 0, opacity: 0, rotation: 360, duration: 2 })
-            .from(".info", { x: "50%", scale: 0, opacity: 0, rotation: 360, duration: 1 })
+        timeLine.from(".welcome", { x: "-50%", scale: 0, opacity: 0, rotation: 360, duration: 1 })
+            .from(".info", { x: "50%", scale: 0, opacity: 0, rotation: 360, duration: 0.5 })
+
     }, [])
     return (
         <div className="test" style={{ textAlign: "center" }}>
-            <p className="welcome" style={{ fontSize: "40px", marginTop: "30px", marginBottom: "30px" }}>Hello Project</p>
+            <p className="welcome " style={{ fontSize: "40px", marginTop: "30px", marginBottom: "30px" }}>Hello Project</p>
             <p className="info" style={{ color: "blue" ,fontSize: "30px"}}>Diệp Vĩnh Kiên</p>
         </div>
     );
